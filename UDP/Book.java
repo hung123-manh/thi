eecebaab1f4c5305689648d68afc566c2d99f863
Manh Hung <lengocmanhhunh@gmail.com>
2026-05-13 23:22:58 +0700

    Add Book class for managing book details

---
+++ UDP/Book.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UDP;
+
+/**
+ *
+ * @author an
+ */
+
+import java.io.Serializable;
+
+public class Book implements Serializable {
+    private static final long serialVersionUID = 20251107L;
+
+    private String id;
+    private String title;
+    private String author;
+    private String isbn;
+    private String publishDate;
+
+    public Book(String id, String title, String author, String isbn, String publishDate) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.publishDate = publishDate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getPublishDate() {
+        return publishDate;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setPublishDate(String publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" + "id=" + id + ", title=" + title + ", author=" + author + ", isbn=" + isbn + ", publishDate=" + publishDate + '}';
+    }
+    
+}
